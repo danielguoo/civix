f91c3f4c3536d051cb985b07d3b9691be24f22b9
michelleduan <mjaneduan@gmail.com>
2018-11-12 22:14:41 -0800

    Dummy calendar and linked main menu
    Dummy calendar - listview w/events 
    -'Learn More' button expands to display more information/fuller description
    -Clicking 'Attending' button will mark green

--- civix-client/src/components/Contact/Contact.js
+++ civix-client/src/components/Contact/Contact.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 
-const Contact = () => <h2>Contact</h2>
-export default Contact
+const flexContainer = {
+  display: 'flex',
+  flexDirection: 'row',
+  padding: 0,
+};
 
-
-
+export default props => (
+  <fragment>
+  <List>
+    <ListItem
+      primaryText="foo1"
+      secondaryText="bar1"/>
+    <ListItem
+      primaryText="foo2"
+      secondaryText="bar2"/>
+  </List>
+  </fragment>
+)
