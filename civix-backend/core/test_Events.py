bb245562626d64cc732d38f5ac5909f0713e36a9
Deven Agrawal <devenagrawal.810@gmail.com>
2018-11-15 20:07:22 -0800

    Testing Scenarios

---
+++ civix-backend/core/test_Events.py
@@ -0,0 +1,119 @@
+from django.test import TestCase, Client
+from rest_framework.test import APIRequestFactory
+from .models import Profile, Event, Item, Post
+
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+class CivixEventsTest(TestCase):
+    '''
+    We use the setUp() function to create the dummy database
+    which shall be used to test our APIs. 
+    '''
+    def setUp(self):
+        self.client = Client()
+
+        e = Event.objects.create(title='Fire in the disco', date=timezone.now(), description='Outta control')
+        Event.objects.create(title='CO2 at the fire', date=timezone.now(), description='In control')
+        Event.objects.create(title='this is boring', date=timezone.now(), description='there is no control in this deterministic universe, alright?')
+
+    '''
+    Success Tests
+    '''
+
+    def test_Events_retrieve(self):
+        res = self.client.get('/events/', follow=True)
+        body = res.json()
+
+        #Success indicates status code of 200, 3 objects in the reply and title of "Fire in the disco"
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(len(body), 3)
+        self.assertEqual(body[0]["title"], 'Fire in the disco')
+
+    def test_Events_create(self):
+        newEvent = {'title': 'Prop 60', 'date':timezone.now(), 'description': 'Legalize ranch'}
+        res = self.client.post('/events/', newEvent, content_type='application/json', follow=True)
+
+        #Success indicates status code of 201, 4 objects in the DB, and title of "Prop 60" in the new object
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(self.getItemsInDB('/events/'), 4)
+        res = self.client.get('/events/4/')
+        body = res.json()
+        self.assertEqual(body["title"],"Prop 60")
+
+    def test_Events_delete(self):
+        res = self.client.delete('/events/3/')
+
+        #Success indicates status code of 204, 2 objects in the DB
+        self.assertEqual(res.status_code, 204)
+        self.assertEqual(self.getItemsInDB('/events/'), 2)
+
+    def test_Events_update(self):
+        updateEvent = {'title': 'No fire', 'date':timezone.now(), 'description': 'Meh'}
+        res = self.client.put('/events/1/', updateEvent, content_type='application/json', follow=True)
+        
+        #Success indicates status code of 200, 3 objects in the DB, and title of "No fire"        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(self.getItemsInDB('/events/'), 3)
+        res = self.client.get('/events/1/')
+        res = res.json()
+        self.assertEqual(res["title"],"No fire")
+
+    '''
+    Failure Tests
+    '''
+
+    # 2 test cases
+    # 1) Wrong URL
+    # 2) Missing user
+    def test_Events_retrieve_failure(self):
+        res1 = self.client.get('/event/', follow=True)
+        res2 = self.client.get('/events/5', follow=True)
+        self.assertEqual(res1.status_code, 404)
+        self.assertEqual(res2.status_code, 404)
+
+    # 2 test cases
+    # 1) Bad request body - missing non-nullable field
+    # 2) Trying to push to an event to particular URL inaccessible to POST requests
+    #Returns a bad request body and doesn't change the number of objects in the table
+    def test_Events_create_failure(self):
+        newEvent = {'title': 'Prop 60', 'description': 'Legalize ranch'}
+        forceNewEvent = {'title': 'Prop 60', 'date':timezone.now(), 'description': 'Legalize ranch'}
+        
+        res1 = self.client.post('/events/', newEvent, content_type='application/json', follow=True)
+        res2 = self.client.post('/events/666', forceNewEvent, content_type='application/json', follow=True)
+        self.assertEqual(res1.status_code, 400)
+        self.assertEqual(res2.status_code, 404)
+
+        self.assertEqual(self.getItemsInDB('/events/'), 3)
+
+    #Post doesn't exist
+    #Returns a missing request message and doesn't change the number of objects in the table
+    def test_Events_delete_failure(self):
+        res = self.client.delete('/events/333/')
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(self.getItemsInDB('/events/'), 3)
+
+    # 2 test cases
+    # 1) Inserting data into Event that doesn't exist
+    # 2) Inserting data into wrong url
+    def test_Events_update_failure(self):
+        rightUpdateEvent = {'title': 'No fire', 'date':timezone.now(), 'description': 'Meh'}
+
+        res1 = self.client.put('/events/666/', rightUpdateEvent, content_type='application/json', follow=True)
+        res2 = self.client.put('/events/', rightUpdateEvent, content_type='application/json', follow=True)
+        self.assertEqual(res1.status_code, 404)
+        self.assertEqual(res2.status_code, 405)
+
+        # Checks if it's the original data
+        self.assertEqual(self.getItemsInDB('/events/'), 3)
+        res = self.client.get('/events/1/')
+        res = res.json()
+        self.assertFalse(res["title"] == "No fire")
+        
+
+    def getItemsInDB(self, url):
+        status = self.client.get(url, follow=True)
+        status = status.json()
+        return len(status)
---
+++ civix-backend/core/test_Profile.py
@@ -0,0 +1,27 @@
+from django.test import TestCase, Client
+from rest_framework.test import APIRequestFactory
+from .models import Profile, Event, Item, Post
+
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+class CivixProfileTest(TestCase):
+    '''
+    We use the setUp() function to create the dummy database
+    which shall be used to test our APIs. 
+    '''
+    def setUp(self):
+        self.client = Client()
+        self.usr = User.objects.create_user('theBurn', 'chaddy@gmail.com', 'banana')
+        usr2 = User.objects.create_user('theWet', 'chaddy@gmail.com', 'fineapple')
+
+        p = Profile.objects.create(user = self.usr, dob=timezone.now(), poliID = 1, 
+        address="10 Downing St", city="NYC", zipcode = "90210", state="WY")
+
+
+    def test_Profile_login(self):
+        self.assertEqual(self.client.login(username='theBurn', password='banana'), True)
+
+    def test_Profile_logout(self):
+        self.assertEqual(self.client.logout(), None)
