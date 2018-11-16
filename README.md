eba62891def544d94f4a8804f8ff5b39b8853369
Daniel Guo <dguo@ucla.edu>
2018-11-16 14:16:18 -0800

    basic changes

--- README.md
+++ README.md
@@ -24,3 +24,11 @@ civix-backend/
 * core/views.py -> methods for handling HTTP requests, serializes JSON data to retrieve and alter objects in database
 * core/models.py -> defines database models for project
 * core/serializers.py -> serializes data for models to be received and sent as JSON
+
+#### Civix- Frontend Structure
+civix-client/src
+* index.js - main entry point for frontend, where React is initialized
+* compenents/ - contains a seperate folder for each component. Each folder contains a Javascript file for the component
+* and its corresponding CSS file
+* components/App - entry component for App, encompasses everything else
+* components/Navbar - uses React Router to creates routes for app.
--- civix-client/src/components/IssuesBoard/IssuesBoard.js
+++ civix-client/src/components/IssuesBoard/IssuesBoard.js
@@ -1,5 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
+import './IssuesBoard.css'
 
-const IssuesBoard = () => <h2>Issues</h2>;
+const candidates2018 = [
+  {office: "California Governor", candidates: [{name: "Gavin Newsom", party: "Democratic"}, {name: "John Cox", party: "Republican"}]},
+  {office: "California Lieutentant Governor", candidates: [{name: "Eleni Kounalakis", party: "Democratic"}, {name: "Ed Hernandez", party: "Democratic"}]},
+  {office: "House of Represententatives, CA-45", candidates: [{name: "Mimi Walters", party: "Republican"}, {name: "Katie Porter", party: "Democratic"}]}
+]
 
-export default IssuesBoard
+const IssuePanel = ({issue: {office, candidates}}) => (
+  <div className="IssuePanel">
+    <h1>{office}</h1>
+    <h2>{candidates[0].name} vs {candidates[1].name}</h2>
+  </div>
+
+)
+
+export default class IssuesBoard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Midterm Election 11/6/2018</h1>
+        <h2>Candidates</h2>
+        {candidates2018.map((el,i) => <IssuePanel key={i} issue={el}/>)}
+        <h2>Propositions</h2>
+      </div>
+    )
+  }
+}
