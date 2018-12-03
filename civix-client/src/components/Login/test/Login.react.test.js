b1152daf4906aac914205756ff02880e868eef58
coolkp <kunjanpatel@kunjans-MacBook-Pro.local>
2018-12-03 05:36:52 -0800

    More Jest/Enzyme

--- civix-client/src/components/Login/test/Login.react.test.js
+++ civix-client/src/components/Login/test/Login.react.test.js
@@ -24,29 +24,4 @@ describe('Login Component', () => {
      expect(wrapper.state('password')).toEqual('cats')
     })
 
-
-  // it('renders a email input', () => {
-  //   expect(shallow(<Login />).find('#email').length).toEqual(1)
-  //  })
-  // it('renders a password input', () => {
-  //   expect(shallow(<Login />).find('#password').length).toEqual(1)
-  //  })
-  //
-  //  describe('Email input', () => {
-  //
-  //   it('should respond to change event and change the state of the Login Component', () => {
-  //    const wrapper = shallow(<Login />)
-  //    wrapper.find('#email').simulate('change', {target: {name: 'email', value: 'blah@gmail.com'}})
-  //    expect(wrapper.state('email')).toEqual('blah@gmail.com')
-  //   })
-  //  })
-  //
-  //  describe('Password input', () => {
-  //
-  //   it('should respond to change event and change the state of the Login Component', () => {
-  //    const wrapper = shallow(<Login />)
-  //    wrapper.find('#password').simulate('change', {target: {name: 'password', value: 'cats'}})
-  //    expect(wrapper.state('password')).toEqual('cats')
-  //   })
-  //  })
 })
